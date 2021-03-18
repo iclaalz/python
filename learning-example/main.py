d42e2ba1c24e2b35e435d9658dfba1680bbbf1b5
iclaalz <ciclal.zeytun@istanbulticaret.edu.tr>
2021-03-19 02:31:17 +0300

    Add files via upload

---
+++ learning-example/main.py
@@ -0,0 +1,38 @@
+a=4+5
+print(a)
+b="iclal"
+c="hilal"
+d=8
+print(a*d)
+len(c) #len -> ifadenin uzunluğunu bulmamı sağlar
+print(len(c))
+#upper-lower harf fonksiyonları
+# upper() : bir string ifadeyi komple büyük yapmaya yarar
+# lower() : bir string ifadeyi komple küçük yapmaya yarar
+c.upper()
+print(c.upper())
+print(c.lower())
+c.isupper()
+print(c.isupper())
+
+#replace metodu : string deki karakterleri değiştirmek için kullanılır
+
+k="iclal"
+print(k.replace("a","e")) # bu kaydedilmiyor
+print(k)
+
+#strip metodu : string deki istenmeyen karakterleri kırpar
+h = "icla l "
+print(h.strip()) #ön tanımlı olarak boşlukları siler
+j="*hilal*"
+print(j)
+print(j.strip("*"))
+
+print(j[2])
+
+m=8
+type(8)
+print(type(8))
+#değişken tipini verir -> type
+
+
